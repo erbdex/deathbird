d099806ab49ce821bae38e96c965b12cb4b39e2a
EC2 Default User <ec2-user@ip-172-31-0-86.ec2.internal>
2015-12-12 19:36:43 +0000

    Added randomized langs

--- fix_logs.py
+++ fix_logs.py
@@ -1,25 +1,28 @@
+from time import sleep, gmtime
+from random import randint
+import calendar, json, ast, sys, yaml
 
-from time import sleep
-import json, ast, sys, yaml
+input_lang = ['hindi', 'tamil', 'hindi', 'tamil', 'malayalam', 'konkani', 'bhojpuri', 'punjabi', 'bhojpuri', 'punjabi', 'bhojpuri', 'punjabi', 'bhojpuri', 'punjabi', 'bhojpuri', 'punjabi', 'marwari', 'marathi', 'marathi', 'marathi']
+output_lang = ['english', 'telugu', 'oriya']
 
 def reformat_log(log):
-	json_acceptable_string = log.replace("'", "\"")
-	json_log = json.loads(json_acceptable_string)
-	json_log['time'] = '1449940902'
-	return json.dumps(json_log)
+        json_acceptable_string = log.replace("'", "\"")
+        json_log = json.loads(json_acceptable_string)
+        json_log['time'] = str(calendar.timegm(gmtime()))
+        print str(randint(0,len(input_lang) - 1))
+        json_log['request']['RequestList']['ecomDataList']['olang'] = input_lang[randint(0,len(input_lang) - 1)]
+        json_log['request']['tlangList'] = output_lang[randint(0,len(output_lang) - 1)]
+        return json.dumps(json_log)
 
 def random_sleep():
-	sleep(0.2)
+        sleep(0.2)
 
 def parse_from_file():
-	reader = open('test.log', 'r')
-	writer = open('out.log', 'a', 0)
-	for line in reader:
-		random_sleep()
-		formatted_log = reformat_log(line)
-		writer.write(formatted_log)
-		
-
-
+        reader = open('test.log', 'r')
+        writer = open('out.log', 'a', 0)
+        for line in reader:
+                random_sleep()
+                formatted_log = reformat_log(line)
+                writer.write(formatted_log)
 
 parse_from_file()
