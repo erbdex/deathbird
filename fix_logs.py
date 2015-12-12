d092b16a1741e598aff324b22f5146731060a87e
Mayank <mayank@thinqbot.com>
2015-12-12 20:09:24 +0000

    Fixed randomized langs & apikeys

--- fix_logs.py
+++ fix_logs.py
@@ -1,20 +1,27 @@
 from time import sleep, gmtime
-from random import randint
+from random import randint, choice
 import calendar, json, ast, sys, yaml
 
-input_lang = ['hindi', 'tamil', 'hindi', 'tamil', 'malayalam', 'konkani', 'bhojpuri', 'punjabi', 'bhojpuri', 'punjabi', 'bhojpuri', 'punjabi', 'bhojpuri', 'punjabi', 'bhojpuri', 'punjabi', 'marwari', 'marathi', 'marathi', 'marathi']
-output_lang = ['english', 'telugu', 'oriya']
+input_lang = 'hindi', 'tamil', 'hindi', 'tamil', 'malayalam', 'konkani', 'bhojpuri', 'punjabi', 'bhojpuri', 'punjabi', 'bhojpuri', 'punjabi', 'bhojpuri', 'punjabi', 'bhojpuri', 'punjabi', 'marwari', 'marathi', 'marathi', 'marathi'
+output_lang = 'english', 'telugu', 'oriya'
+api_keys = ['44615f2b6ddbc6228c254673148ac0425642', '44615f2b6ddbc6228c254673148ac0425642', 'cf2bc00a6caafdacbac53d59ad214ef08f80', 'cf2bc00a6caafdacbac53d59ad214ef08f80', '2c358efbae4c0a7f35be91a0a2ec06f7b2ab', '2c358efbae4c0a7f35be91a0a2ec06f7b2ab', '2c358efbae4c0a7f35be91a0a2ec06f7b2ab', '2c358efbae4c0a7f35be91a0a2ec06f7b2ab', '2c358efbae4c0a7f35be91a0a2ec06f7b2ab', '709e4fcd7df7ef1f8cc5a860afc7ca7fc7c3', 'e0cf8c563d5d1de9ad3bfbffc4ac3f902ff1', '4a4dd6549e777b5fffebe0d63131ee56f81a', '056c7f80f6fd7ac4cba2aca429dc94bf0f5b', 'f540ac51bcccdc1065f159af9eef382a50db']
+
 
 def reformat_log(log):
+        rand_api_key = choice(api_keys)
         json_acceptable_string = log.replace("'", "\"")
         json_log = json.loads(json_acceptable_string)
         json_log['time'] = str(calendar.timegm(gmtime()))
-        json_log['request']['RequestList'][0]['ecomDataList'][0]['olang'] = input_lang[randint(0,len(input_lang) - 1)]
-        json_log['request']['tlangList'] = output_lang[randint(0,len(output_lang) - 1)]
+        json_log['request']['RequestList'][0]['ecomDataList'][0]['olang'] = choice(input_lang)
+        json_log['request']['tlangList'] = choice(output_lang)
+        json_log['request']['apiKey'] = rand_api_key
+        json_log['apikey'] = rand_api_key
+        json_log['response']['map']['apikey'] = rand_api_key
+        print json.dumps(json_log)
         return json.dumps(json_log)
 
 def random_sleep():
-        sleep(0.2)
+        sleep(0.01)
 
 def parse_from_file():
         reader = open('test.log', 'r')
@@ -22,6 +29,6 @@ def parse_from_file():
         for line in reader:
                 random_sleep()
                 formatted_log = reformat_log(line)
-                writer.write(formatted_log)
+                writer.write(formatted_log + '\n')
 
 parse_from_file()
