892d0cb178c051271ea9df2fc401e1f6980b95d9
Abhishek Tiwari <erbdex@gmail.com>
2015-12-15 12:43:40 +0530

    removed older flow, new one's skeleton in place

--- fix_logs.py
+++ fix_logs.py
@@ -58,42 +58,41 @@ def reformat_log(logged_line):
 def random_sleep():
     sleep(0.01 * randint(0, 50))
 
-def read_new_lines_from_modified_source(src):
-    know_where_we_left = '__magic__'
-    reader = open(src, 'r')
-    pass
 
-def write_new_lines_to_target(target):
-    pass
+def read_new_lines_from_modified_source(src, target):
+    # = '__magic__ally know where we left. Next step: make this persistent.'
+    know_where_we_left = 0
+    reader = open(src, 'r')
+    # line wise counter or character wise(?) Assuming line wise for now.
+    reader.seek(know_where_we_left)
+
+    # Gather and push across as many lines as possible-
+    for line in reader:
+        random_sleep()
+        formatted_log = reformat_log(line)
+        write_new_lines_to_target(formatted_log, target)
+
+
+def write_new_lines_to_target(log, target):
+    writer = open(target, 'a', 0)
+    writer.write(log + '\n')
+
 
 def file_modified_do_your_shit(src, target):
-    read_new_lines_from_modified_source(src)
-    write_new_lines_to_target(target)
+    read_new_lines_from_modified_source(src, target)
+
 
 def initiate_watchdog(src, target):
     # Triggers call_handler_file_edited in case the file undergoes mods.
     file_modified_do_your_shit(src, target)
+
 
 def parse_from_file():
     file_to_read_from = sys.argv[1]
     file_to_write_to  = sys.argv[2]
 
     initiate_watchdog(file_to_read_from, file_to_write_to)
-
-
     # Flow ends here(?).
-
-    reader = open(file_to_read_from, 'r')
-    writer = open(file_to_write_to, 'a', 0)
-    try:
-        reader.seek(0)
-        for line in reader:
-             random_sleep()
-             formatted_log = reformat_log(line)
-             print formatted_log
-             writer.write(formatted_log + '\n')
-    except Exception as e:
-        print 'Raised: {0}'.format(e)
 
 if __name__ == '__main__':
     parse_from_file()
