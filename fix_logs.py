4c69727c8a984d53ad630637a5a0fdca23bd14c6
Abhishek Tiwari <erbdex@gmail.com>
2015-12-15 11:15:53 +0530

    more verbose comments, added exception checks

--- fix_logs.py
+++ fix_logs.py
@@ -1,46 +1,73 @@
-from time import sleep, gmtime
-from random import randint, choice
-import calendar, json, ast, sys, yaml
-
-input_lang = 'hindi', 'tamil', 'hindi', 'tamil', 'malayalam', 'konkani', 'bhojpuri', 'punjabi', 'bhojpuri', 'punjabi', 'bhojpuri', 'punjabi', 'bhojpuri', 'punjabi', 'bhojpuri', 'punjabi', 'marwari', 'marathi', 'marathi', 'marathi'
-output_lang = 'english', 'telugu', 'oriya', 'english', 'english', 'english', 'urdu', 'urdu', 'urdu', 'bengali'
-api_keys = '44615f2b6ddbc6228c254673148ac0425642', '44615f2b6ddbc6228c254673148ac0425642', 'cf2bc00a6caafdacbac53d59ad214ef08f80', 'cf2bc00a6caafdacbac53d59ad214ef08f80', '2c358efbae4c0a7f35be91a0a2ec06f7b2ab', '2c358efbae4c0a7f35be91a0a2ec06f7b2ab', '2c358efbae4c0a7f35be91a0a2ec06f7b2ab', '2c358efbae4c0a7f35be91a0a2ec06f7b2ab', '2c358efbae4c0a7f35be91a0a2ec06f7b2ab', '709e4fcd7df7ef1f8cc5a860afc7ca7fc7c3', 'e0cf8c563d5d1de9ad3bfbffc4ac3f902ff1', '4a4dd6549e777b5fffebe0d63131ee56f81a', '056c7f80f6fd7ac4cba2aca429dc94bf0f5b', 'f540ac51bcccdc1065f159af9eef382a50db', '88fe888d0b693eb5ab5fca7aefe94ac5a6cd', '7fe412e62ecfb2a9ca3766319b192eeb90be', '7fe412e62ecfb2a9ca3766319b192eeb90be', '7fe412e62ecfb2a9ca3766319b192eeb90be', '7fe412e62ecfb2a9ca3766319b192eeb90be', '7fe412e62ecfb2a9ca3766319b192eeb90be'
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 
-def reformat_log(log):
-        rand_api_key = choice(api_keys)
-        json_acceptable_string = log.replace("'", "\"")
-        json_log = {}
-        try:
-            json_log = json.loads(json_acceptable_string)
+from time import sleep
+from random import randint
+import json, sys
 
-            #FIX TIME
-            json_log['time'] = str(calendar.timegm(gmtime()))
 
-            # FIX REQUEST
-            json_log['request']['RequestList'][0]['ecomDataList'] = json_log['request']['RequestList'][0]['ecomDataList'][0]
-            json_log['request']['RequestList'] = json_log['request']['RequestList'][0]
-            json_log['request']['tlangList'] = json_log['request']['tlangList'][0]
+def fix_log_timestamp(time_in_log):
+    # Converting time to a second epoch.
+    epoch = time_in_log / 1000
+    return epoch
 
-            #FIX RESPONSE
-            json_log['response']['map']['data']['myArrayList'] = json_log['response']['map']['data']['myArrayList'][0]['myArrayList'][0]
-            print '.',
-        except:
-            print '0',
-        sys.stdout.flush()
-        return json.dumps(json_log)
+def fix_nested_objects(json_log):
+    '''
+        Flattening request object nested within array so that Kibana can micro-analyse it.
+        Doesn't do it for docs like- [{...},{...}]
+        See- https://github.com/elastic/kibana/issues/998
+    '''
+    json_log['request']['RequestList'][0]['ecomDataList'] = json_log['request']['RequestList'][0]['ecomDataList'][0]
+    json_log['request']['RequestList'] = json_log['request']['RequestList'][0]
+    json_log['request']['tlangList'] = json_log['request']['tlangList'][0]
+
+    json_log['response']['map']['data']['myArrayList'] = json_log['response']['map']['data']['myArrayList'][0]['myArrayList'][0]
+    return json_log
+
+
+def extract_json_from_log(log):
+    # The logged line is-- "2015-12-11 08:17:34 - {..valid..json..}\n"
+    delimiter = '- '
+    return log.split(delimiter)[1]
+
+
+
+def reformat_log(logged_line):
+    log = extract_json_from_log(logged_line)
+    json_acceptable_string = log.replace("'", "\"")
+    json_log = dict()
+    try:
+        json_log = json.loads(json_acceptable_string)
+
+        # The log right now logs the second epoch with the last three ms digits appended. Translates to ~47000 AD.
+        timestamp_logged = json_log['time']
+        json_log['time'] = fix_log_timestamp(timestamp_logged)
+
+        # Flattening request object nested within array so that Kibana can micro-analyse it.
+        fixed_log = fix_nested_objects(json_log)
+    except Exception as e:
+        print 'Error: \"{0}\" Trace: \"{1}\"'.format(log, e)
+
+    return json.dumps(fixed_log)
+
 
 def random_sleep():
-        sleep(0.01 * randint(0, 50))
+    sleep(0.01 * randint(0, 50))
+
 
 def parse_from_file():
-        reader = open(sys.argv[1], 'r')
-        writer = open(sys.argv[2], 'a', 0)
-        while True:
-            reader.seek(0)
-            for line in reader:
-                 random_sleep()
-                 formatted_log = reformat_log(line)
-                 writer.write(formatted_log + '\n')
+    reader = open(sys.argv[1], 'r')
+    writer = open(sys.argv[2], 'a', 0)
+    try:
+        reader.seek(0)
+        for line in reader:
+             random_sleep()
+             formatted_log = reformat_log(line)
+             print formatted_log
+             writer.write(formatted_log + '\n')
+    except Exception as e:
+        print 'Raised: {0}'.format(e)
 
-parse_from_file()
+if __name__ == '__main__':
+    parse_from_file()
